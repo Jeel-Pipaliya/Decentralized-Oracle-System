1ad874dba4e4c6894620338be02df5c763775623
Jeel Pipaliya <pipaliyajeelj@gmail.com>
2026-02-06 18:21:22 +0530

    Crop Insurance Contract

--- contracts/CropInsurance.sol
+++ contracts/CropInsurance.sol
@@ -1,6 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+interface IWeatherOracle {
+    function getFinalWeather() external view returns(uint, uint);
+}
+
 contract CropInsurance {
-    // Crop Insurance contract code
+    address public farmer;
+    address public oracle;
+    uint public thresholdRainfall;
+    bool public paid;
+
+    constructor(address _oracle, uint _thresholdRainfall) payable {
+        farmer = msg.sender;
+        oracle = _oracle;
+        thresholdRainfall = _thresholdRainfall;
+        paid = false;
+    }
+
+    function checkAndPay() public {
+        require(!paid, "Already paid");
+
+        (, uint rainfall) = IWeatherOracle(oracle).getFinalWeather();
+
+        if(rainfall < thresholdRainfall) {
+            paid = true;
+            payable(farmer).transfer(address(this).balance);
+        }
+    }
+
+    receive() external payable {}
 }
