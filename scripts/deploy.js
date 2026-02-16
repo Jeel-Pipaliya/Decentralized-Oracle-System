c2441ae0600decba27955bf3cb3326ac4e8b4394
Jeel Pipaliya <pipaliyajeelj@gmail.com>
2026-02-16 22:02:51 +0530

    add Deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,28 @@
-// Deployment script
+const hre = require("hardhat");
+
 async function main() {
-  console.log("Deploying contracts...");
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying with account:", deployer.address);
+
+  const WeatherOracle = await hre.ethers.getContractFactory("WeatherOracle");
+  const oracle = await WeatherOracle.deploy();
+  await oracle.deployed();
+
+  console.log("WeatherOracle deployed to:", oracle.address);
+
+  const CropInsurance = await hre.ethers.getContractFactory("CropInsurance");
+  const insurance = await CropInsurance.deploy(oracle.address, 10, {
+    value: hre.ethers.utils.parseEther("1"),
+  });
+
+  await insurance.deployed();
+  console.log("CropInsurance deployed to:", insurance.address);
+
+  console.log("Done!");
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
